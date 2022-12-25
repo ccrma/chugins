46f6060644f76fc49294e81ff2982c6c380246fa
nshaheed <nicholasshaheed@gmail.com>
2022-12-25 07:23:49 -0800

    Add equivalency test (currently failing)

---
+++ rave/rave_test.ck
@@ -0,0 +1,8 @@
+Pan2 p => rave r => blackhole;
+
+r.load(me.dir() + "rave_chafe_data_rt.ts");
+r.method("forward");
+
+<<< me.dir() >>>;
+
+while (1::second => now) {};
--- rave/rave_test.cpp
+++ rave/rave_test.cpp
@@ -1,5 +1,8 @@
 #include <CppUnitTest.h>
 #include "rave.cpp"
+#include <math.h>
+
+#define PI 3.14159265
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -7,9 +10,87 @@ namespace ravetests
 {
 	TEST_CLASS(ravetests)
 	{
-		TEST_METHOD(Basic)
+	public:
+		// Is forward(x) == decode(encode(x))?
+		TEST_METHOD(Equivalent)
 		{
-			Rave r = Rave(44100);
+			Rave forward = Rave(44100);
+			Rave encode  = Rave(44100);
+			Rave decode  = Rave(44100);
+
+			// load model and method...
+			Assert::IsTrue(forward.load("rave_chafe_data_rt.ts"));
+			Assert::IsTrue(forward.setMethod("forward"));
+
+			encode.load("rave_chafe_data_rt.ts");
+			encode.setMethod("forward");
+
+			decode.load("rave_chafe_data_rt.ts");
+			decode.setMethod("decode");
+
+			int channels = 64;
+			int buffer = 2048;
+
+			SAMPLE* in = new SAMPLE[channels * buffer]();
+			SAMPLE* out = new SAMPLE[channels * buffer]();
+			SAMPLE* outEncode = new SAMPLE[channels * buffer]();
+			SAMPLE* outDecode = new SAMPLE[channels * buffer]();
+
+			for (int i = 0; i < buffer; i++) {
+				// 220hz sine wave
+				in[i*channels] = sin(2 * PI * 220 * i / 44100);
+			}
+
+			forward.perform(in, out, buffer);
+
+			encode.perform(in, outEncode, buffer);
+			decode.perform(outEncode, outDecode, buffer);
+
+			// forward and encode/decode of the same input should
+			// yield approx the same output (as forward is defined 
+			// as forward = decode(encode(x))
+			for (int i = 0; i < channels * buffer; i++) {
+				Assert::AreEqual(out[i], outDecode[i], (float)0.000001, 
+					std::to_wstring(i / channels).c_str());
+			}
+		}
+		// TODO: implement
+		// The same as Equivalent, but streaming (like how it will
+		// work in practice)
+		TEST_METHOD(EquivalentLong) {
+			Rave forward = Rave(44100);
+			Rave encode = Rave(44100);
+			Rave decode = Rave(44100);
+
+			// load model and method...
+			Assert::IsTrue(forward.load("rave_chafe_data_rt.ts"));
+			Assert::IsTrue(forward.setMethod("forward"));
+
+			encode.load("rave_chafe_data_rt.ts");
+			encode.setMethod("forward");
+
+			decode.load("rave_chafe_data_rt.ts");
+			decode.setMethod("decode");
+
+			int channels = 64;
+			int buffer = 1;
+
+			SAMPLE* in = new SAMPLE[channels]();
+			SAMPLE* out = new SAMPLE[channels]();
+			SAMPLE* outEncode = new SAMPLE[channels]();
+			SAMPLE* outDecode = new SAMPLE[channels]();
+
+			// do this across a larger range of samples...
+			for (int i = 0; i < 10000; i++) {
+				// 220hz sine wave
+				in[i] = sin(2 * PI * 220 * i / 44100);
+
+				forward.perform(in, out, buffer);
+
+				encode.perform(in, outEncode, buffer);
+				decode.perform(outEncode, outDecode, buffer);
+
+			}
 		}
 	};
 }
